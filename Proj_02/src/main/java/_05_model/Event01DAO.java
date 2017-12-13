709a94ed6d49c6fb6487f9cc239c57a1924d1f09
黃仕元 <andy@eeit9830>
2017-12-13 14:42:01 +0800

    第一次push
    My Google Maps Demo2

---
+++ Proj_02/src/main/java/_05_model/Event01DAO.java
@@ -0,0 +1,10 @@
+package _05_model;
+
+import java.util.List;
+
+public interface Event01DAO {
+
+	List<Event02> select();
+	
+	List<Event02> selectMusic();
+}
---
+++ Proj_02/src/main/webapp/SQL_command/EventPro.sql
@@ -0,0 +1,40 @@
+Use Proj02
+drop table event01
+create table event01(
+[EventID]  integer primary key,
+[EventName]	Nvarchar(max),
+[ShowGroupName]	Nvarchar(max),
+[EventTypeID]	Nvarchar(max),
+[DurationStart]	datetime,
+[DurationEnd]	datetime,
+[IsCharge]	integer,
+[Fee]	Nvarchar(max),
+[ShoppingUrl]	Nvarchar(max),
+[ContactName]	Nvarchar(max),
+[ContactTel]	nvarchar(max),
+[ContactFax]	Nvarchar(max),
+[BriefIntroduction]	Nvarchar(max),
+[vContent]	Nvarchar(max),
+[EventUrl]	Nvarchar(max),
+[YouTubeUrl]	Nvarchar(max),
+[ImageFile]	varchar(max),
+[LogoImageFile]	varchar(max),
+[InsertTime]	Datetime,
+[dtStart]	Datetime,
+[dtEnd]	Datetime,
+[timeStart]	time,
+[Location]	Nvarchar(max),
+[CityID]	nVarchar(100),
+[AreaID]	nVarchar(100), 
+[Address]	Nvarchar(max),
+[longitude]	Varchar(100),
+[latitude]	Varchar(100),
+[collectionCount] integer  DEFAULT '0',
+[shareCount]  integer  DEFAULT '0',
+[isBlock]     integer  DEFAULT '0' 
+
+
+)
+
+
+
