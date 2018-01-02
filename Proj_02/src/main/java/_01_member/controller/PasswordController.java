f2196749663956c851f8f32d3d65e30367251387
梁玉蓁 <eeit9829@EEIT>
2018-01-02 10:36:55 +0800

    01021036
    01021036

--- Proj_02/src/main/java/_01_member/controller/PasswordController.java
+++ Proj_02/src/main/java/_01_member/controller/PasswordController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import _01_member.EmailUtil;
+import _01_member.model.MemberBean;
 import _01_member.model.MemberService;
 
 @Controller
-@RequestMapping(path = { "/_01_blog/password.controller" })
+@RequestMapping(path = { "/_01_member/password.controller" })
 public class PasswordController {
 
 	@Autowired
@@ -17,8 +19,24 @@ public class PasswordController {
 	
 	@RequestMapping(method = { RequestMethod.POST }, produces = { "application/json;charset=UTF-8" })
 	@ResponseBody
-	public boolean check() {
-		
-		return false;
+	public boolean check(MemberBean bean, String memberemail) {
+		System.out.println("password.controller");
+		boolean result= memberService.checkAccount(memberemail);
+		if(result) {
+			bean.setMemberemail(memberemail);
+			String randompassword= memberService.getRandomPassword();
+			
+			String sendEmail = EmailUtil.sendEmail(memberemail, "您的新密碼",
+			 "<h1>新密碼</h1><br>您的新密碼為："+randompassword+"<br>請重新登入變更密碼",null);
+			System.out.println(".sendEmail:"+sendEmail);
+			
+			bean.setMemberpassword(randompassword);
+			memberService.changepsw(bean);
+			System.out.println("success");			
+		}else {		
+			System.out.println("error");
+			return false;
+		}
+		return true;
 	}
 }
